561f9980e99f718d15e8f4e6348fe7183993e84b
Bobby Johnson <bobby.johnson@auth0.com>
2019-01-31 12:29:42 -0800

    added cli sde with a run command

---
+++ bin/run/index.js
@@ -0,0 +1,27 @@
+var Chalk = require('chalk');
+var Cli = require('structured-cli');
+
+module.exports = Cli.createCommand('run', {
+  description: 'Starts the emulator',
+  options: {
+    'path': {
+      alias: 'p',
+      required: true,
+      description: 'The path to your Stream Deck Plugin directory.',
+      dest: 'path',
+      type: 'string'
+    },
+    'executable': {
+      alias: 'e',
+      required: true,
+      description: 'The executable file name for your Stream Deck Plugin.',
+      dest: 'executable',
+      type: 'string'
+    }
+  },
+  handler: handleRun
+});
+
+function handleRun(args) {
+  console.log(Chalk.bold(`Running ${args.executable} located in ${args.path}`));
+}
